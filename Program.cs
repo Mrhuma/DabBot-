ec6935eab89d33a58431401c9cccd5dedc2e058e
Alexander <alexhaas123@gmail.com>
2021-12-21 12:18:27 -0700

    Basic connection done

--- Program.cs
+++ Program.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace DabBot_
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        private DiscordSocketClient _client;
+        public static Task Main(string[] args) => new Program().MainAsync();
 
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+#if DEBUG
+            string token = File.ReadAllText(@"..\token.txt");
+#else
+            string token = File.ReadAllText("token.txt");
+#endif
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.Ready += Ready;
+
+            //Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task Ready()
+        {
+            return Task.CompletedTask;
         }
     }
 }
