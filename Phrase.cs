b29faa20b3daf4826f4145d404e20b287634463e
Alexander <alexhaas123@gmail.com>
2021-12-21 12:27:36 -0700

    Added Phrase object

---
+++ Phrase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DabBot_
+{
+    class Phrase
+    {
+        private string _text;
+        private int _mediaCount;
+
+        public string text { get { return _text; } set { _text = value; } }
+        public int mediaCount { get { return _mediaCount; } set { _mediaCount = value; } }
+
+        public Phrase(string text, int mediaCount)
+        {
+            this.text = text;
+            this.mediaCount = mediaCount;
+        }
+    }
+}
